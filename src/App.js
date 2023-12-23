23ecc8e750b87082262c6e655bdf5d40378dcc70
ngunjiritris2019 <140059988+ngunjiritris2019@users.noreply.github.com>
2023-12-23 16:21:00 +0300

    Update styling

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
       <Weather />
 
       <footer>
-        Coded by Beatrice Ngunjiri, open-source on{" "}
+        Coded by Beatrice Ngunjiri, open-sourced on{" "}
         <a
           href="https://github.com/ngunjiritris2019/my-first-react-app"
           target="_blank"
--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,48 @@
 import React from "react";
+import "./Weather.css";
 
-function Weather() {}
+function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control "
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-secondary" />
+          </div>
+        </div>
+      </form>
+      <h1>Nairobi</h1>
+      <ul>
+        <li>Sat 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6 forecast">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly cloudy"
+          />
+
+          <span className="temperature">6 </span>
+          <span className="unit">°C| °F</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 25%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Weather;
