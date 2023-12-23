61c6c030eed09b0843a96abae4499aa0eadbc691
ngunjiritris2019 <140059988+ngunjiritris2019@users.noreply.github.com>
2023-12-23 14:51:51 +0300

    Remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <Weather />
-      </header>
+      <h1>Weather App</h1>
+      <Weather />
+
       <footer>
         Coded by Beatrice Ngunjiri, open-source on{" "}
         <a
