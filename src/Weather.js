30d2cf66d14eda55c1fbf611291223c91f171154
ngunjiritris2019 <140059988+ngunjiritris2019@users.noreply.github.com>
2023-12-24 08:56:14 +0300

    Use the correct icon url

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
-import "./Weather.css";
 
-function Weather() {
+import "./Weather.css";
+import ForecastInfo from "./ForecastInfo";
+
+function Weather(props) {
   const [forecast, setForecast] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function displayForecast(response) {
     setForecast({
@@ -14,15 +16,30 @@ function Weather() {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       city: response.data.name,
-      icon: response.data.weather[0].icon,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
     });
+  }
+
+  function search() {
+    const apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCitySearch(event) {
+    setCity(event.target.value);
   }
 
   if (forecast.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -30,6 +47,7 @@ function Weather() {
                 placeholder="Enter a city"
                 className="form-control "
                 autoFocus="on"
+                onChange={handleCitySearch}
               />
             </div>
             <div className="col-3">
@@ -41,37 +59,11 @@ function Weather() {
             </div>
           </div>
         </form>
-        <h1>Nairobi</h1>
-        <ul>
-          <li>
-            <FormattedDate date={forecast.date} />
-          </li>
-          <li className="text-capitalize">{forecast.description}</li>
-        </ul>
-        <div className="row">
-          <div className="col-6 forecast">
-            <img src={forecast.icon} alt={forecast.description} />
-            <span className="temperature">
-              {Math.round(forecast.temperature)}
-            </span>
-            <span className="unit">°C| °F</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: </li>
-              <li>Humidity: {forecast.humidity}%</li>
-              <li>Wind: {Math.round(forecast.wind)}km/h</li>
-            </ul>
-          </div>
-        </div>
+        <ForecastInfo data={forecast} />
       </div>
     );
   } else {
-    const apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
-    let city = "Nairobi";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayForecast);
-
+    search();
     return "Loading...";
   }
 }
