9f4834d65e36da6ec68dcfb3f8e579cb9a1479f0
ngunjiritris2019 <140059988+ngunjiritris2019@users.noreply.github.com>
2024-01-04 18:19:27 +0300

    Update styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+    const sunriseHour = 6; // Adjust this to the actual sunrise hour
+    const sunsetHour = 18; // Adjust this to the actual sunset hour
+
+    // Check if it's day or night based on current time
+    const isDay = currentHour >= sunriseHour && currentHour < sunsetHour;
+
+    // Set the background image based on day or night
+    setBackgroundImage(
+      isDay ? "images/nairobi-day.jpeg" : "./images/nairobi-night.jpeg"
+    );
+  }, []);
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <div className="container">
         <Weather defaultCity="Nairobi" />
 
